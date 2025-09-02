dcc48f7ce38e8f62067fde916ee6019c3fd528a0
kerikir <vlasov-kerik@mail.ru>
2025-09-02 21:41:37 +0300

    implemented adapter for recycle view

---
+++ app/src/main/java/com/example/listofhistoricalfigures/UserAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.listofhistoricalfigures
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+// Создает список и наполняет данными
+internal class UserAdapter: RecyclerView.Adapter<UserAdapter.UserHolder>()
+{
+    // Описание элементов и привязка к Recycle View
+    class UserHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    {
+        // Scope-function
+        /** Связывает данные пользователя с элементом интерфейса */
+        fun bind(user: User) = with(itemView)
+        {
+            val tvName: TextView = findViewById(R.id.userName)
+            val tvDescriptor: TextView = findViewById(R.id.userDescription)
+
+            tvName.text = user.name
+            tvDescriptor.text = user.description
+        }
+    }
+
+
+    private var users: List<User> = ArrayList<User>()
+
+    // Создаем View Holder и инициализирум View для списка
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserHolder
+    {
+        return UserHolder(LayoutInflater.from(parent.context)
+            .inflate(R.layout.user_item, parent, false))
+    }
+
+    override fun getItemCount(): Int
+    {
+        return users.size
+    }
+
+    // Связываем View с содержимым
+    override fun onBindViewHolder(viewHolder: UserHolder, position: Int)
+    {
+        viewHolder.bind(users[position])
+    }
+
+    /** Передаем данные и опопвещаем адаптер об обновлении данных списка */
+    fun refreshUsers(users: List<User>) : Unit
+    {
+        this.users = users
+        notifyDataSetChanged()
+    }
+}
