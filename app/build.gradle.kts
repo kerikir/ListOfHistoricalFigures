eb4ccb36fba760739766ef14e00299ba1bf11777
kerikir <vlasov-kerik@mail.ru>
2025-09-02 17:16:01 +0300

    dependencies connected

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,4 +44,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.android.support:recyclerview-v7:28.0.0")
+    implementation("android.arch.lifecycle:extensions:1.1.1")
 }
