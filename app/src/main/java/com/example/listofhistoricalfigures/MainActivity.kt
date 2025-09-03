a96c442706caa86a24cf5dd3424c135b5fdc4fe5
kerikir <vlasov-kerik@mail.ru>
2025-09-04 00:00:16 +0300

    added action bar

--- app/src/main/java/com/example/listofhistoricalfigures/MainActivity.kt
+++ app/src/main/java/com/example/listofhistoricalfigures/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 
 import android.view.Menu
 import android.view.MenuItem
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
